3bd25fb905e244b76f7de364b64a005b347b72e7
Darius Carter <dcarterbusinessemail@gmail.com>
2018-11-16 13:06:28 -0600

    Design

---
+++ RollerCw/app/src/main/java/com/example/student/rollercw/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.student.rollercw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.CheckBox;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
